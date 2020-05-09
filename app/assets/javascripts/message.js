88101d4ec78308fc78b771e0b8fe7766daa9d8ef
strongmasahiro <masahiro.hirano@di-v.co.jp>
2020-05-09 15:13:50 +0900

    非同期修正#2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,17 +40,17 @@ $(function(){
    };
  }
 $('.chat-main__message-form__form').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
   .done(function(data){
     var html = buildHTML(data);
     $('.chat-main__message-list').append(html);
